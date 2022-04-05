e3bf1b84d56e1bebedde4c1c7dcd6821dc3ce8ab
ElsePaul <93398086+ElsePaul@users.noreply.github.com>
2022-04-05 14:03:49 +0530

    Add files via upload

---
+++ bot.php
@@ -0,0 +1,18 @@
+<?php
+    date_default_timezone_set("Asia/kolkata");
+    //Data From Webhook
+    $content = file_get_contents("php://input");
+    $update = json_decode($content, true);
+    $chat_id = $update["message"]["chat"]["id"];
+    $message = $update["message"]["text"];
+    $message_id = $update["message"]["message_id"];
+    $id = $update["message"]["from"]["id"];
+    $username = $update["message"]["from"]["username"];
+    $firstname = $update["message"]["from"]["first_name"];
+    $start_msg = $_ENV['START_MSG']; 
+
+if($message == "/start"){
+    send_message($chat_id,$message_id, "***Hey $firstname \nUse !bin xxxxxx to Check BIN \n$start_msg***");
+}
+
+?>
